885382df17aa5d8aba9d4085d42a1016019ec10e
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2023-12-04 00:18:16 -0300

    se modifico algo

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ for(var x = 0; x <links.length;x++){
     }
 }
 
+var menu_visible2 = false;
+let menu2 = document.getElementById("nav2");
+function mostrarOcultarMenu2(){
+    if(menu_visible2==false){//si esta oculto
+        menu2.style.display = "block";
+        menu_visible2 = true;
+    }
+    else{
+        menu2.style.display = "none";
+        menu_visible2 = false;
+    }
+}
+// oculto el menu una vez que selecciono una opcion
+let links2 = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu2.style.display = "none";
+        menu_visible2  = false;
+    }
+}
 
 //carrousel de fotos
 
