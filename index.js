97fcb84e52502eba7c4bf66d530cdfe7da60a334
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2023-12-25 22:54:08 -0300

    se modifico todo

--- index.js
+++ index.js
@@ -75,4 +75,59 @@ function nextSlide() {
 
 startInterval(); // Inicia el carrusel automáticamente al cargar la página
 
+        // Variables globales
+        var scene, camera, renderer, cube;
 
+        function init() {
+            // Crea una escena
+            scene = new THREE.Scene();
+
+            // Crea una cámara
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            camera.position.z = 5;
+
+            // Crea un renderizador
+            renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+
+            // Crea una geometría de cubo (box)
+            var geometry = new THREE.BoxGeometry(100, 100, 100);
+
+            // Crea un material con la textura de estrellas
+            var texture = new THREE.TextureLoader().load('./img/cielo-estrellado.jpg');
+            var material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+
+            // Crea la malla con la geometría y el material
+            cube = new THREE.Mesh(geometry, material);
+
+            // Añade el cubo a la escena
+            scene.add(cube);
+
+            // Maneja el cambio de tamaño de la ventana
+            window.addEventListener('resize', onWindowResize, false);
+
+            // Inicia la animación
+            animate();
+        }
+
+        function animate() {
+            requestAnimationFrame(animate);
+
+            // Rota el cubo para el efecto 3D
+            cube.rotation.x += 0.005;
+            cube.rotation.y += 0.005;
+
+            // Renderiza la escena
+            renderer.render(scene, camera);
+        }
+
+        function onWindowResize() {
+            // Ajusta el tamaño del renderizador cuando cambia el tamaño de la ventana
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        }
+
+        // Inicializa la aplicación
+        init();
